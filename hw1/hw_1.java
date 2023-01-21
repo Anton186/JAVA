58c7072ca109a30537c77a360c7cd48c0c73fa7c
Toxa <black2604@mail.ru>
2023-01-21 22:51:10 +0500

    hw_done

---
+++ hw1/hw_1.java
@@ -0,0 +1,16 @@
+// +Написать программу вычисления n-ого треугольного числа. url
+package hw1;
+
+import java.util.Scanner;
+
+public class hw_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int number = scan.nextInt();
+        long res = 0;
+        res = number*(number+1)/2;
+        System.out.println("Вычисление " + number + " треугольного числа, равно: " + res);
+
+    }
+}
