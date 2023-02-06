da4b46f964233a4599cc73dfbc15fa528dac6a98
Toxa <black2604@mail.ru>
2023-02-06 23:47:02 +0500

    hw5_1_done

--- hw5/hw_1.java
+++ hw5/hw_1.java
@@ -1,6 +1,7 @@
 //Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что один человек может иметь несколько телефонов.
 package hw5;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,19 +10,26 @@ import java.util.Scanner;
 
 public class hw_1 {
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();  // объява листа
-        Scanner scan = new Scanner(System.in); // оъява сканера
-        Map<String, List<Integer>> phoneBook = new HashMap<>(); // объява тел книги
-        System.out.println("Введите Фио"); // запрос на фио
-        String fio = scan.nextLine(); // запись в переменную
-        System.out.println("Введите номер телефона"); // запрос на телефон
-        int number = scan.nextInt(); // запись в переменную
-        phoneBook.put(fio, list); // перезаписывает
-        if (phoneBook.containsKey(fio)) {
-            list.clear();
-            list.add(phoneBook.keySet(fio));
-            
+        List<Integer> list = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+        Map<String, ArrayList<Integer>> phoneBook = new HashMap<>();
+        String fio = null;
+        while(true) {
+            System.out.println("Введите Фио, для остановки напишите stop");
+            fio = scan.nextLine();
+            if(fio.equals("stop")){
+                System.out.println("Телефонная книга заполнена следующими значениями: " + phoneBook.entrySet());
+                break;
+            }
+            System.out.println("Введите номер телефона");
+            int number = Integer.parseInt(scan.nextLine());
+            if (phoneBook.containsKey(fio)) {
+                phoneBook.computeIfAbsent(fio, k -> new ArrayList<>()).add(number);
+            } else {
+                phoneBook.computeIfAbsent(fio, k -> new ArrayList<>()).add(number);
+            }
         }
+        scan.close();
 
     }
 }
--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -6,19 +7,33 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>(); // объява листа
+        List<Integer> list = new ArrayList<Integer>(); // объява листа
         Scanner scan = new Scanner(System.in); // оъява сканера
-        Map<String, Integer> phoneBook = new HashMap<>(); // объява тел книги
-        System.out.println("Введите Фио"); // запрос на фио
-        String fio = scan.nextLine(); // запись в переменную
-        System.out.println("Введите номер телефона"); // запрос на телефон
-        int number = scan.nextInt(); // запись в переменную
-        //phoneBook.put(fio, list); // перезаписывает
-        phoneBook.put("первый", 1);
-        phoneBook.put("второй", 2);
-        phoneBook.put("третий", 3);
-        phoneBook.put("четвертый", 4);
-        System.out.println(phoneBook.values());
-
+        Map<String, ArrayList<Integer>> phoneBook = new HashMap<>(); // объява тел книги
+        String fio = null;
+        do {
+            System.out.println("Введите Фио, для остановки напишите stop"); // запрос на фио
+            fio = scan.nextLine(); // запись в переменную
+            fio = scan.nextLine(); // запись в переменную
+            System.out.println("Введите номер телефона"); // запрос на телефон
+            int number = scan.nextInt(); // запись в переменную
+            if (phoneBook.containsKey(fio)) {
+                //list.clear();
+                System.out.println(list.toString()+" - пустой лист");
+                //list = phoneBook.get(fio);
+                //System.out.println(list.toString()+" - лист равен фонбук с таким фио");
+                //list.add(list.size(), number);
+                //System.out.println(list.toString() + " добавлен новый номер к фио");
+                phoneBook.computeIfAbsent(fio, k -> new ArrayList<>()).add(number);
+                System.out.println(phoneBook.get(fio)+ "новые телефоны этого фио");
+            } else {
+                //list.clear();
+                //list.add(list.size(), number);
+                phoneBook.computeIfAbsent(fio, k -> new ArrayList<>()).add(number);
+                System.out.println(list.toString() + " создан фио и номер");
+            }
+            System.out.println(phoneBook.entrySet());
+        } while (!"fio".equals("stop"));
+        scan.close();
     }
 }
